ab3434df362e3a7929cdd0115070dca709a1123f
Jakub Piskorz <jakub.piskorz@gazeta.pl>
2024-10-06 01:43:15 +0200

    file system :D

--- file/file.asm
+++ file/file.asm
@@ -3,14 +3,14 @@ section .data
 ; Expressions used for conditional assembly
     CREATE      equ 1
     OVERWRITE   equ 1
-    APPENT      equ 1
+    APPEND      equ 1
     O_WRITE     equ 1
     READ        equ 1
     O_READ      equ 1
     DELETE      equ 1
     
 ; Syscall symbols
-    NR_READ     equ 0
+    NR_read     equ 0
     NR_write    equ 1
     NR_open     equ 2
     NR_close    equ 3
@@ -65,7 +65,7 @@ section .data
     successRead     db  "Reading file", NL, 0
     successPosition db  "Positioned in file", NL, 0
     
-section.bss
+section .bss
     buffer  resb    bufferlen
 section .text
     global main
@@ -131,3 +131,271 @@ main:
     mov rsp, rbp
     pop rbp
     ret
+    
+    %ENDIF
+    
+    %IF O_WRITE
+    ; OPEN AND OVERWRITE AT AN OFFSET IN A FILE, THEN CLOSE ----
+    ; Open file to write
+    mov rdi, fileName
+    call openFile
+    mov qword [FD], rax
+    
+    ; Position file at offset
+    mov rdi, qword [FD]
+    mov rsi, qword [len2] ; offset at this location
+    mov rdx, 0
+    call positionFile
+    
+    ; write to file
+    mov rdi, qword [FD]
+    mov rsi, txt4
+    mov rdx, qword [len4]
+    call writeFile
+    
+    ; close file
+    mov rdi, qword [FD]
+    call closeFile
+    
+    %ENDIF
+    %IF READ
+    ; OPEN AND READ FROM A FILE, THEN CLOSE ------------
+    ; Open file to read
+    mov rdi, fileName
+    call openFile
+    mov qword [FD], rax
+    
+    ; Read from file
+    mov rdi, qword [FD]
+    mov rsi, buffer
+    mov rdx, bufferlen
+    call readFile
+    mov rdi, rax
+    call printString
+    
+    ; Close file
+    mov rdi, qword [FD]
+    call closeFile
+    
+    %ENDIF
+    %IF O_READ
+    ; OPEN AND READ AT AN OFFSET FROM A FILE, THEN CLOSE ----
+    ; open file to read
+    mov rdi, fileName
+    call openFile
+    mov qword [FD], rax
+    
+    ; position file at offeset
+    mov rdi, qword [FD]
+    mov rsi, qword [len2] ; Skip the first line
+    mov rdx, 0
+    call positionFile
+    
+    ; Read from file
+    mov rdi, qword [FD]
+    mov rsi, buffer
+    mov rdx, 10 ; Number of characters to read
+    call readFile
+    mov rdi, rax
+    call printString
+    
+    ; Close file
+    mov rdi, qword [FD]
+    call closeFile
+    
+    %ENDIF
+    
+    %IF DELETE
+    ; DELETE A FILE ----------------------------------------
+    ; Delete file UNCOMMENT NEXT LINES TO USE
+        mov rdi, fileName
+        call deleteFile
+    %ENDIF
+    
+    leave
+    ret
+    
+    ; FILE MANIPULATION FUNCTIONS --------------------------
+    
+    ; ------------------------------------------------------
+    global readFile
+    ; Arguments:
+    ; rdi: file descriptor
+    ; rsi: buffer
+    ; rdx: number of characters to read
+    readFile:
+        mov rax, NR_read
+        syscall ; rax contains # of characters read
+        cmp rax, 0
+        jl readError
+        mov byte [rsi+rax], 0 ; Add a terminating zero
+        mov rax, rsi
+        
+        mov rdi, successRead
+        push rax ; Save rax
+        call printString
+        pop rax  ; Return previous rax
+        ret
+    readError:
+        mov rdi, errorRead
+        call printString
+        ret
+    ; Return arguments:
+    ; rdi: text from file
+    
+    ; ------------------------------------------------------
+    global deleteFile
+    ; Arguments:
+    ; rdi, file descriptor
+    deleteFile:
+        mov rax, NR_unlink
+        syscall
+        cmp rax, 0
+        jl deleteError
+        mov rdi, successDelete
+        call printString
+        ret
+    deleteError:
+        mov rdi, errorDelete
+        call printString
+        ret
+    ; Return arguments
+    ; rdi: success or error msg
+        
+    ; ------------------------------------------------------
+    global appendFile
+    ; Arguments:
+    ; rdi: name of file
+    appendFile:
+        mov rax, NR_open
+        mov rsi, O_RDWR|O_APPEND
+        syscall
+        cmp rax, 0
+        jl appendError
+        mov rdi, successAppend
+        push rax
+        call printString
+        pop rax
+        ret
+    appendError:
+        mov rdi, errorAppend
+        call printString
+        ret
+        
+    ; ------------------------------------------------------
+    global openFile
+    ; Arguments:
+    ; rdi: name of file
+    openFile:
+        mov rax, NR_open
+        mov rsi, O_RDWR
+        syscall
+        cmp rax, 0
+        jl openError
+        mov rdi, successOpen
+        push rax
+        call printString
+        pop rax
+        ret
+    openError:
+        mov rdi, errorOpen
+        call printString
+        ret
+    ; Return arguments:
+    ; rax: file descriptor
+        
+    ; -------------------------------------------------------
+    global writeFile
+    ; Arguments:
+    ; rdi: file descriptor
+    ; rsi: text to write
+    ; rdx: length of string to write
+    writeFile:
+        mov rax, NR_write
+        syscall
+        cmp rax, 0
+        jl writeError
+        mov rdi, successWrite
+        call printString
+        ret
+    writeError:
+        mov rdi, errorWrite
+        call printString
+        ret
+        
+    ; -------------------------------------------------------
+    global positionFile
+    ; Arguments:
+    ; rdi: file descriptor
+    ; rsi: offet length
+    ; rdx: ???    
+    positionFile:
+        mov rax, NR_lseek
+        syscall
+        cmp rax, 0
+        jl positionError
+        mov rdi, successPosition
+        call printString
+        ret
+    positionError:
+        mov rdi, errorPosition
+        call printString
+        ret
+        
+    ; -------------------------------------------------------
+    global closeFile
+    ; Arguments:
+    ; rdi: file descriptor
+    closeFile: 
+        mov rax, NR_close
+        syscall
+        cmp rax, 0
+        jl closeError
+        mov rdi, successClose
+        call printString
+        ret
+    closeError:
+        mov rdi, errorClose
+        call printString
+        ret
+        
+    ; -------------------------------------------------------
+    global createFile
+    ; Arguments:
+    ; rdi: file name
+    createFile: 
+        mov rax, NR_create
+        mov rsi, S_IRUSR | S_IWUSR
+        syscall
+        cmp rax, 0
+        jl createError
+        mov rdi, successCreate
+        push rax
+        call printString
+        pop rax
+        ret
+    createError:
+        mov rdi, errorCreate
+        call printString
+        ret
+        
+    ; PRINT -------------------------------------------------
+    global printString
+    printString:
+    ; count characters
+        mov r12, rdi
+        mov rdx, 0
+    strLoop:
+        cmp byte [r12], 0
+        je strDone
+        inc rdx
+        inc r12
+        jmp strLoop
+    strDone:
+        cmp rdx, 0
+        je prtDone
+        mov rsi, rdi
+        mov rdi, 1
+        syscall
+    prtDone:
+        ret

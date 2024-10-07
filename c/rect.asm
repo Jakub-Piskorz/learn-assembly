4d45b990567d025b5639b8808f8553f2ed4cbc89
Jakub Piskorz <jakub.piskorz@gazeta.pl>
2024-10-07 20:54:43 +0200

    wip

---
+++ c/rect.asm
@@ -0,0 +1,23 @@
+;r ect.asm
+section .data
+section .bss
+section .text
+global rarea
+rarea:
+      section .text
+            push  rbp
+            mov   rbp, rsp
+            mov   rax, rdi
+            imul  rsi
+            leave
+            ret
+global rcircum
+rcircum:
+      section .text
+            push  rbp
+            mov   rbp, rsp
+            mov   rax, rdi
+            add   rax, rsi
+            imul  rax, 2
+            leave
+            ret

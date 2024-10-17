82bdce1e8b4403ced37dff654fee619797c10e46
Jakub Piskorz <jakub.piskorz@gazeta.pl>
2024-10-17 23:19:47 +0200

    a bit cheesy, but works

--- scan-string/scan-string.asm
+++ scan-string/scan-string.asm
@@ -1,73 +1,135 @@
-; scan-string.asm
+; strings.asm
 extern printf
-
 section .data
-    str1    db  "This is first string.", 10, 0
-    str2    db  "This is second string.", 10, 0
-    strlen2 equ $-str2-2
-    str21   db  "Comparing strings: strings do not differ", 10, 0
-    str22   db  "Comparing strings: strings do differ",
-            db  "String from position: %d.", 10, 0
-    str3    db  "Pchnąć w tę łódź jeża lub ośm skrzyń fig.", 0
-    strlen3 equ $-str3-2
-    str33   db  "Now look at this string: %s", 10, 0
-    str4    db  "z", 0
-    str44   db  "Found character '%s' at position: %d.", 10, 0
-    str45   db  "Didn't find character '%s'.", 10, 0
-    str46   db  "Scanning for character '%s'.", 10, 0
-
+      string1    db "This is the 1st string.",10,0
+      string2    db "This is the 2nd string.",10,0
+      strlen2    equ $-string2-2
+      string21   db "Comparing strings: The strings do not differ.",10,0
+      string22   db "Comparing strings: The strings differ, "
+                 db "starting at position: %d.",10,0
+      string3    db "The quick brown fox jumps over the lazy dog.",0
+      strlen3    equ $-string3-2
+      string33   db "Now look at this string: %s",10,0
+      string4    db "z",0
+      string44   db "The character '%s' was found at position: %d.",10,0
+      string45   db "The character '%s' was not found.",10,0
+      string46   db "Scanning for the character '%s'.",10,0
+section .bss
 section .text
-global main
+      global main
 main:
-    push rbp
-    mov rbp, rsp
-    
-    ; Let's print 2 strings
-    xor rax, rax
-    mov rdi, str1
-    call printf
-    mov rdi, str2
-    call printf
-    
-    ; Compare 2 strings
-    lea rdi, [str1]
-    lea rsi, [str2]
-    mov rdx, strlen2
-    call compare1
-    cmp rax, 0
-    jnz notequal1
-    ; String are equal, print
-    mov rdi, str21
-    call printf
-    jmp otherversion
-    
-    mov rsp, rbp
-    pop rbp
-    ret
-    
-    ; FUNCTIONS
-    
-    ; Compare 2 strings
-    compare1:   mov rcx, rdx
-                cld
-    cmpr:       cmpsb
-                jne notequal1
-                loop cmpr
-                xor rax, rax
-                ret
-    notequal1:  mov rax, strlen2
-                dec rdx
-                sub rax, rcx
-                ret
-                xor rax, rax
-                ret
-                
-    ; Compare 2 strongs again
-    compare2:   mov rcx, rdx
-                cld
-                repe cmpsb
-                je equal2
-                mov rax, strlen2
-                sub rax, rcx
-    equal2:     xor rax, rax
-                ret    
+      push   rbp
+      mov    rbp,rsp
+; print the 2 strings
+      xor    rax,rax
+      mov    rdi, string1
+      call   printf
+      mov    rdi, string2
+      call   printf
+; compare 2 strings -----------------------------------------------
+      lea    rdi,[string1]
+      lea    rsi,[string2]
+      mov    rdx, strlen2
+      call   compare1
+      cmp    rax,0
+      jnz    not_equal1
+;strings are equal, print
+      mov    rdi, string21
+      call   printf
+      jmp    otherversion
+;strings are not equal, print
+not_equal1:
+      mov    rdi, string22
+      mov    rsi, rax
+      xor    rax,rax
+      call   printf
+; compare 2 strings, other verstion ------------------------------
+otherversion:
+      lea    rdi,[string1]
+      lea    rsi,[string2]
+      mov    rdx, strlen2
+      call   compare2
+      cmp    rax,0
+      jnz    not_equal2
+;strings are equal, print
+      mov    rdi, string21
+      call   printf
+      jmp    scanning
+;strings are not equal, print
+not_equal2:
+      mov    rdi, string22
+      mov    rsi, rax
+      xor    rax,rax
+      call   printf
+; scan for a character in a string -------------------------------
+; first print the string
+      mov    rdi,string33
+      mov    rsi,string3
+      xor    rax,rax
+      call   printf
+; then print the search argument, can only be 1 character
+      mov    rdi,string46
+      mov    rsi,string4
+      xor    rax,rax
+      call   printf
+scanning:
+      lea    rdi,[string3]     ;      string
+      lea    rsi,[string4]     ;      search argument
+      mov    rdx, strlen3
+      call   cscan
+      cmp    rax,0
+      jz     char_not_found
+;character found, print
+      mov    rdi,string44
+      mov    rsi,string4
+      mov    rdx,rax
+      xor    rax,rax
+      call   printf
+      jmp    exit
+;character not found, print
+char_not_found:
+      mov    rdi,string45
+      mov    rsi,string4
+      xor    rax,rax
+      call   printf
+exit:
+leave
+ret
+; FUNCTIONS ===============================================================
+; function compare 2 strings ------------------------------------
+compare1:    mov    rcx, rdx
+             cld
+cmpr:        cmpsb
+             jne    notequal
+             loop   cmpr
+             xor    rax,rax
+             ret
+notequal:    mov    rax, strlen2
+             dec    rcx           ;compute position
+             sub    rax,rcx       ;compute position
+             ret
+             xor    rax,rax
+             ret
+;----------------------------------------------------------------
+; function compare 2 strings ------------------------------------
+compare2:    mov    rcx, rdx
+             cld
+             repe   cmpsb
+             je     equal2
+             mov    rax, strlen2
+             sub    rax,rcx       ;compute position
+             ret
+equal2:      xor    rax,rax
+             ret
+;----------------------------------------------------------------
+;function scan a string for a character
+cscan:       mov    rcx, rdx
+             lodsb
+             cld
+             repne  scasb
+             jne    char_notfound
+             mov    rax, strlen3
+             sub    rax,rcx       ;compute position
+             ret
+char_notfound:      xor rax,rax
+             ret
